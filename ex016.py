0cd1cd9d5aa81dfd1523eca8e46e75afee1cc173
João-Medina <jvmedina2003@gmail.com>
2023-04-06 02:33:13 -0300

    Update ex016.py

--- ex016.py
+++ ex016.py
@@ -1,22 +1,6 @@
-assinatura = str(input("Insira o tipo de assinatura: \n[ Basic    ] \n[ Silver   ] \n[ Gold     ] \n[ Platinum ] \n")).lower()
-
-while True:
-    if assinatura == "basic":
-        nivel = 0.3
-        break
-    elif assinatura == "silver":
-        nivel = 0.2
-        break
-    elif assinatura == "gold":
-        nivel = 0.1
-        break
-    elif assinatura == "platinum":
-        nivel = 0.05
-        break
-    else:
-        assinatura = str(input("ERRO! Digite uma assinatura válida: "))
-
-faturamento = int(input("Insira o faturamento anual: "))
-bonus = faturamento * nivel
-
-print("O bônus tem um valor de R${:.2f}".format(bonus))
+ano = 1989
+nome = "Luke Skywalker"
+saldo = 50.30
+print(("O tipo da variável ano é {}".format(type(ano))))
+print(("O tipo da variável nome é {}".format(type(nome))))
+print(("O tipo da variável saldo é {}".format(type(saldo))))
